8e572d75f0e4dfb8a0591120b978a70393b4edec
Souto <jasvbetico@hotmail.com>
2019-06-04 23:47:19 +0200

    calzado dejeme

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -6,7 +6,9 @@ import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -15,6 +17,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = @Index(columnList = "home_team, visitor_team"))
 public class Game extends DomainEntity {
 
 	private Date	gameDate;
